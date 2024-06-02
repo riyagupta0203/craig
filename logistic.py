ce189fe91e192f5f6f07d7cb6e37552d8a7d6d9b
riyagupta0203 <96865627+riyagupta0203@users.noreply.github.com>
2024-06-02 11:20:45 +0530

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -1,3 +1,4 @@
+import glob
 import argparse
 import numpy as np
 import time
@@ -7,13 +8,11 @@ from warnings import simplefilter
 # Ignore all future warnings
 simplefilter(action='ignore', category=FutureWarning)
 np.seterr(all='ignore')
-
+import util
 import random
-
 
 def sigmoid(x):
     return 1. / (1 + np.exp(-x))
-
 
 def softmax(x):
     e = np.exp(x - np.max(x))  # prevent overflow
@@ -21,7 +20,6 @@ def softmax(x):
         return e / np.sum(e, axis=0)
     else:
         return e / np.sum(e, axis=1, keepdims=True)  # ndim = 2
-
 
 class LogisticRegression(object):
     def __init__(self, dim, num_class):
@@ -61,9 +59,7 @@ class LogisticRegression(object):
         d_b = -np.mean(d_y, axis=0, keepdims=True)
         return np.array([d_W, d_b])
 
-
 class Optimizer(object):
-
     @staticmethod
     def order_elements(shuffle, n, seed=1234):
         if shuffle == 0:
@@ -151,7 +147,6 @@ class Optimizer(object):
             T[epoch] = (time.process_time() - start_epoch)
         return W, T
 
-
 def load_dataset(dataset, normalize=False):
     DATASET_DIR = '/tmp/data/'
     if dataset == 'covtype':
@@ -160,6 +155,9 @@ def load_dataset(dataset, normalize=False):
         covertype = fetch_ucirepo(id=31) 
         X = covertype.data.features.to_numpy()
         y = covertype.data.targets.to_numpy()
+
+        # Convert to binary classification for simplicity: class 2 and above -> class 1
+        y = (y > 1).astype(int)
 
         N = len(X)
         NUM_TRAINING = N // 2  # Using integer division
@@ -189,6 +187,7 @@ def load_dataset(dataset, normalize=False):
         X_0, y_0 = X_train[y_train == 0], y_train[y_train == 0]
         X_1, y_1 = X_train[y_train == 1], y_train[y_train == 1]
         X_2, y_2 = X_train[y_train == 2], y_train[y_train == 2]
+
         X_1, y_1 = X_1[:len(X_2)], y_1[:len(X_2)]
         X_train = np.vstack([X_1, X_2])
         y_train = np.hstack([y_1, y_2])
@@ -197,6 +196,7 @@ def load_dataset(dataset, normalize=False):
         X_0, y_0 = X_test[y_test == 0], y_test[y_test == 0]
         X_1, y_1 = X_test[y_test == 1], y_test[y_test == 1]
         X_2, y_2 = X_test[y_test == 2], y_test[y_test == 2]
+
         X_1, y_1 = X_1[:len(X_2)], y_1[:len(X_2)]
         X_test = np.vstack([X_1, X_2])
         y_test = np.hstack([y_1, y_2])
@@ -214,33 +214,27 @@ def load_dataset(dataset, normalize=False):
         X_val = scaler.transform(X_val)
         X_test = scaler.transform(X_test)
 
-    print(f'Loading completed')
+    print('Loading completed')
     return X_train, y_train, X_val, y_val, X_test, y_test
 
-
 if __name__ == '__main__':
-    import util
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', type=str, default='covtype', help='Dataset type')
-    parser.add_argument('--method', type=str, default='sgd', help='Optimization method')
-    parser.add_argument('-s', type=float, default=0.1, help='Step size for learning rate')
-    parser.add_argument('--greedy', type=int, default=0, help='Shuffle type')
+    parser.add_argument('--shuffle', type=int, default=0, help='Shuffle type')
+    parser.add_argument('--dataset', type=str, default='covtype', help='Dataset type')
     parser.add_argument('--epoch', type=int, default=100, help='Number of epochs')
     parser.add_argument('--reg', type=float, default=0.0, help='L2 regularization term')
+    parser.add_argument('--method', type=str, default='sgd', help='Optimization method')
     args = parser.parse_args()
 
-    X_train, y_train, X_val, y_val, X_test, y_test = load_dataset(args.data, normalize=True)
-    print(f'Dataset: {args.data}\nMethod: {args.method}\nShuffle: {args.greedy}\nEpoch: {args.epoch}\n')
+    X_train, y_train, X_val, y_val, X_test, y_test = load_dataset(args.dataset, normalize=True)
+    print(f'Dataset: {args.dataset}\nMethod: {args.method}\nShuffle: {args.shuffle}\nEpoch: {args.epoch}\n')
 
-    y_train_one_hot = np.zeros((len(y_train), 2))
-    y_train_one_hot[np.arange(len(y_train)), y_train] = 1
+    num_classes = np.unique(y_train).size
+    num_classes = 1 if num_classes == 2 else num_classes  # Handle binary and multi-class
 
-    y_val_one_hot = np.zeros((len(y_val), 2))
-    y_val_one_hot[np.arange(len(y_val)), y_val] = 1
-
-    y_test_one_hot = np.zeros((len(y_test)), 2)
-    y_test_one_hot[np.arange(len(y_test)), y_test] = 1
+    y_train_one_hot = np.eye(num_classes)[y_train] if num_classes > 1 else y_train.reshape(-1, 1)
+    y_val_one_hot = np.eye(num_classes)[y_val] if num_classes > 1 else y_val.reshape(-1, 1)
+    y_test_one_hot = np.eye(num_classes)[y_test] if num_classes > 1 else y_test.reshape(-1, 1)
 
     num_class = y_train_one_hot.shape[1]
     num_feature = X_train.shape[1]
@@ -251,9 +245,9 @@ if __name__ == '__main__':
     optimizer = Optimizer()
 
     weights = np.ones(len(X_train))
-    step = args.s
+    step = 1e-1
     lr = [step / np.sqrt(i + 1) for i in range(args.epoch)]
-    params = optimizer.optimize(args.method, model, X_train, y_train_one_hot, weights, args.epoch, args.greedy, lr, args.reg)
+    params = optimizer.optimize(args.method, model, X_train, y_train_one_hot, weights, args.epoch, args.shuffle, lr, args.reg)
     W, T = params
 
     acc = []
